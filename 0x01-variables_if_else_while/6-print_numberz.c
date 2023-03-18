69907aa64e28356fc2c83e53e36f32ce7783e644
Your Name <maymohamed614@gmail.com>
2023-03-17 18:46:05 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int Numberz;
+
+for (Numberz = 0; Numberz < 10; Numberz++)
+{
+putchar('0' + Numberz);
+}
+putchar('\n');
+return (0);
+}
