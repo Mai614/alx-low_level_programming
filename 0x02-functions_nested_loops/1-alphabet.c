ce1d1b7651858e6cf79603e4f325476057e991f6
Your Name <maymohamed614@gmail.com>
2023-03-21 13:56:51 -0700

    the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ *print the alphabet, in lowercase
+ */
+void print_alphabet(void)
+{
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+	_putchar('\n');
+}
