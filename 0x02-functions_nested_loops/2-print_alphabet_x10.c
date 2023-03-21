592cda4035afbba9d9f58dd17d4ab614b72e39db
Your Name <maymohamed614@gmail.com>
2023-03-21 15:35:21 -0700

    islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * Description:
+ * print_alphabet - Prints the alphabet in lowercase from a to z
+ *
  *      This function prints the lowercase alphabet from 'a' to 'z' ten times,
  *      using the _putchar function. It does this by first initializing a loop
  *      that will run ten times, and then for each iteration of that loop, it
