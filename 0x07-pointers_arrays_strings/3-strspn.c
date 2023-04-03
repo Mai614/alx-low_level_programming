3e16d2f702d8a2fbff6a5c6c7b66128530030431
Your Name <maymohamed614@gmail.com>
2023-04-03 16:50:52 -0700

    function that gets the length of a prefix substring.

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: pointer to a string to be scanned
- * @accept: pointer to a string containing the characters to match
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to a string to be scanned.
+ * @accept: Pointer to a string containing the characters to match.
  *
- * Return: the number of bytes in the initial segment of `s`
- *         which consist only of bytes from `accept`.
+ * Return: The number of bytes in the initial segment of `s`
+ * which consist only of bytes from `accept`.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j;
+unsigned int i, j, len = 0;
 
 for (i = 0; s[i] != '\0'; i++)
 {
@@ -17,14 +17,15 @@ for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
+len++;
 break;
-}
 }
 }
 if (accept[j] == '\0')
 {
-return (i);
+return (len);
 }
-return (i);
+}
+return (len);
 }
 
