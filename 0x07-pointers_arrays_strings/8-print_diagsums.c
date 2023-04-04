c2bd6cf4f56bef608dd1277c7f501801b9f143d9
Your Name <maymohamed614@gmail.com>
2023-04-03 18:53:18 -0700

    function that prints the sum of the two diagonals of a square matrix of integers.

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagsums - Prints the sum of the
+ * two diagonals of a square matrix of integers.
+ * @a: Pointer to an integer array that represents the square matrix.
+ * @size: The size of the square matrix.
+ *
+ * Return: nothing.
+ */
+void print_diagsums(int *a, int size)
+{
+int i;
+int sum1 = 0, sum2 = 0;
+
+for (i = 0; i < size; i++)
+{
+sum1 += *(a + i * size + i);
+sum2 += *(a + i * size + (size - i - 1));
+}
+
+printf("Sum of diagonal from top-left to bottom-right: %d\n", sum1);
+printf("Sum of diagonal from bottom-left to top-right: %d\n", sum2);
+}
+
