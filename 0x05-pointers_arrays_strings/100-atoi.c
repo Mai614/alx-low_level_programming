020c7743acf7741db4993fb82a3e9f1a07f9c4b8
Your Name <maymohamed614@gmail.com>
2023-04-09 06:25:11 -0700

    lib

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+s = 8;
+return (s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int _atoi(char *s);
 void rev_string(char *s);
 int _putchar(char c);
 void reset_to_98(int *n);
