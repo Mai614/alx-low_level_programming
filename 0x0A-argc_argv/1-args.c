79562fa521783aa3d9c428bbd1e8ba92e0b1b9d5
Your Name <maymohamed614@gmail.com>
2023-04-10 15:39:26 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - entery point
+ *@argc: count of the arguments
+ *@argv: an array of pointers to the strings
+ *Return: always 0
+ */
+
+int main(int argc, char *argv[]__attribute__ ((unused)))
+{
+
+printf("%d\n", argc - 1);
+
+return (0);
+
+}
