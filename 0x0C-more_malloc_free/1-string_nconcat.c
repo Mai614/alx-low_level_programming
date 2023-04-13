62c168e71d82e51dcfe4777913e8c53097f9e5fe
Your Name <maymohamed614@gmail.com>
2023-04-13 15:28:28 -0700

    concatinate

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+#include <string.h>
+/**
+ * string_nconcat - Concatenates two strings up to a certain number of bytes
+ * @s1: The first string to concatenate
+ * @s2: The second string to concatenate
+ * @n: The maximum number of bytes of s2 to concatenate
+ *
+ * Return: If successful - a pointer to the concatenated string
+ *         If failure - NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+size_t s1_len, s2_len;
+char *concatinate;
+
+if (s1 == NULL)
+s1 = " ";
+
+if (s2 == NULL)
+s2 = " ";
+
+s1_len = strlen(s1);
+s2_len = strlen(s2);
+
+if (n >= s2_len)
+{
+n = s2_len;
+}
+
+concatinate = (char *) malloc(s1_len + n + 1);
+
+if (concatinate == NULL)
+{
+return (NULL);
+}
+
+
+memcpy(concatinate, s1, s1_len);
+memcpy(concatinate + s1_len, s2, n);
+
+return (concatinate);
+
+}
