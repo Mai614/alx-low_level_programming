828e6da32ddccef02f9285b66dd703f8de1b7a58
mai <maymohamed614@gmail.com>
2023-05-02 19:08:40 -0700

    --

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,25 +11,33 @@
 size_t print_listint_safe(const listint_t *head)
 {
 const listint_t *current = head, *next;
+
 size_t count = 0;
 
 while (current != NULL)
 {
+
 next = current->next;
-printf("[%p] %d\n", (void *)&(*current), current->n);
+printf("[%p] %d\n",
+(void *)&(*current), current->n);
+
 count++;
 
 if (current <= next)
 {
+
 printf("-> [%p] %d\n", (void *)&(*next), next->n);
 break;
+
 }
 
 current = next;
+
 }
 
 if (current == NULL)
 return (count);
 
 exit(98);
+
 }
